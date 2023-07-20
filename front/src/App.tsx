10e41600f31ccad7a6b3c222ae777e48ab9e0c45
oussama adaoumoum <oussamaadm755@gmail.com>
2023-07-20 18:34:15 +0100

    adding the Ant design table

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
 import { useStateContext } from "./contexts"
-
+import TableGrid from "./components/Table"
 function App() {
 
   const { darkMode } = useStateContext()
@@ -10,7 +10,8 @@ function App() {
       <div className='dark:bg-app bg-gray-100 w-screen min-h-screen relative flex flex-col items-center pl-[85px] pr-[15px] pt-[75px] '>
         <Sidebar />
         <Navbar />
-        <div className="bg-gray-300/10 w-full h-72 rounded-xl" />
+        {/* <div className="bg-gray-300/10 w-full h-72 rounded-xl" /> */}
+        <TableGrid />
       </div>
     </div>
   )
