96f4a6028d4817001b2f2b45a8fa8568500e888d
oussamaAdaoumoum <oussamaadm755@gmail.com>
2023-07-28 22:11:06 +0100

    adding tha antd Calendar, but u need to connect it to the backend

--- front/src/pages/Tracking/index.tsx
+++ front/src/pages/Tracking/index.tsx
@@ -1,10 +1,44 @@
+import React, { useState } from 'react';
+import { Alert, Calendar, Modal } from 'antd';
+import type { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
+
 function Tracking() {
+  const [value, setValue] = useState(() => dayjs('2017-01-25'));
+  const [selectedValue, setSelectedValue] = useState(() => dayjs('2017-01-25'));
+  const [modalVisible, setModalVisible] = useState(false);
 
+  const onSelect = (newValue: Dayjs) => {
+    setValue(newValue);
+    setSelectedValue(newValue);
+    showModal(); // Show the modal when a date is selected
+  };
 
-    return (
-      <div>
-          <h1>Tracking</h1>
-      </div>
-    );
-  }
-  export default Tracking;
+  const onPanelChange = (newValue: Dayjs) => {
+    setValue(newValue);
+  };
+
+  const showModal = () => {
+    setModalVisible(true);
+  };
+
+  const hideModal = () => {
+    setModalVisible(false);
+  };
+
+  return (
+    <>
+      <Alert message={`You selected date: ${selectedValue?.format('YYYY-MM-DD')}`} />
+      <Calendar value={value} onSelect={onSelect} onPanelChange={onPanelChange} />
+      <Modal
+        open={modalVisible}
+        onCancel={hideModal}
+        footer={null}
+      >
+        <p>You selected date: {selectedValue?.format('YYYY-MM-DD')}</p>
+      </Modal>
+    </>
+  );
+}
+
+export default Tracking;
